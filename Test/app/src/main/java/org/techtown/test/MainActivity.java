a265632fd880ee506997ecd35711cd97f7703152
JinHyeong <truelight92@gmail.com>
2022-11-22 10:53:14 +0900

    first commit

---
+++ Test/app/src/main/java/org/techtown/test/MainActivity.java
@@ -0,0 +1,17 @@
+package org.techtown.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
